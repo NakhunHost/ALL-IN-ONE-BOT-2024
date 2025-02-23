8edd2edded2d2743e67712fc8727d5a70ceee9a2
NakhunHost <presidentialrealmsstudio@gmail.com>
2025-02-23 18:16:00 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -13,7 +13,7 @@ module.exports = {
         ];
 
      
-        const statuses = ['online', 'online', 'online'];
+        const statuses = ['online'];
 
      
         let currentActivityIndex = 0;
